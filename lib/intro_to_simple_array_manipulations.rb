59542b3a9b9b30f9254e92eaeb8f8f154b2dbf95
Shawn Alexander <shawn.k.alexander@gmail.com>
2020-05-23 15:31:58 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,21 @@
+def using_concat(array, arraytwo)
+  array.concat(arraytwo)
+end 
+def using_insert(array, arraytwo)
+  array.insert(4,arraytwo)
+end 
+def using_uniq(array)
+array.uniq
+end
+
+def using_flatten(array)
+  array.flatten 
+end 
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, number)
+  array.delete_at(number)
+end 
